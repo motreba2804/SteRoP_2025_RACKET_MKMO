6d0bf0736eec94d99e421a7cbd2e04596d5fbb54
Mateusz Kargol <mateusz.kargol1@gmail.com>
2025-12-09 13:50:26 +0100

    Implementacja nadawania danych o obrocie i położeniu platformy przez UART do STM32

--- Pico/src/main.c
+++ Pico/src/main.c
@@ -2,12 +2,17 @@
 #include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "hardware/uart.h"
 #include "icm20948.h"
 #include "platform.h"
 
 #define SDA_PIN 16
 #define SCL_PIN 17
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
+
 #define I2C_PORT i2c0
+#define UART_PORT uart0
 
 int main() {
     stdio_init_all();
@@ -20,6 +25,13 @@ int main() {
     gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(SDA_PIN);
     gpio_pull_up(SCL_PIN);
+
+    // UART
+    uart_init(UART_PORT, 115200);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    gpio_disable_pulls(UART_TX_PIN);
+    gpio_disable_pulls(UART_RX_PIN);
 
     icm20948_init();
     sleep_ms(100);
@@ -50,9 +62,16 @@ int main() {
 
         motion_update(&state, ax, ay, az, gx, gy, gz, dt, k_pitch, k_roll, angle_deadzone, v_max);
 
-        printf("pitch=%.2f deg | roll=%.2f deg | x=%.3f m | y=%.3f m\n",
-               state.pitch*180.0f/M_PI, state.roll*180.0f/M_PI, state.x, state.y);
+        char buf[100];
+        int len = snprintf(buf, sizeof(buf),
+                       "Pitch: %.2f\tRoll: %.2f\tPos: x: %.2f, y: %.2f\n",
+                       state.pitch, state.roll, state.x, state.y);
 
+        // Wysyłka po UART
+        uart_write_blocking(UART_PORT, (const uint8_t*)buf, len);
+    
+        printf("pitch=%.2f deg | roll=%.2f deg | x=%.3f m | y=%.3f m\n",
+               state.pitch*180.0f/M_PI, state.roll*180.0f/M_PI, state.x, state.y);    
         sleep_ms(10);
     }
 }
